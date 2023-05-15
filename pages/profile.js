6fcc3127a2547cbbc097996e978ab30e89f60124
김진영 <jinyoung01099@gmail.com>
2023-05-15 18:40:08 +0900

    [Fix] 클라이언트 사이드 페이지 가드에서 서버 사이드 페이지 가드로 코드 수정

--- pages/profile.js
+++ pages/profile.js
@@ -1,7 +1,25 @@
+import { getSession } from "next-auth/client";
 import UserProfile from "../components/profile/user-profile";
 
 function ProfilePage() {
   return <UserProfile />;
+}
+
+export async function getServerSideProps(context) {
+  const session = await getSession({ req: context.req });
+  console.log(session);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/auth",
+        // permanent는 이 redirection이 영구적용인지 임시적용인지를 알려줌
+        permanent: false,
+      },
+    };
+  }
+
+  return { props: { session } };
 }
 
 export default ProfilePage;
