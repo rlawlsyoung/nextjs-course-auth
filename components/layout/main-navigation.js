4bf64f27f774a75ecbd78f8b7643a6b35a766b3a
김진영 <jinyoung01099@gmail.com>
2023-05-15 17:10:34 +0900

    [Add] 로그아웃 기능 추가

--- components/layout/main-navigation.js
+++ components/layout/main-navigation.js
@@ -1,12 +1,16 @@
 import Link from "next/link";
-import { useSession } from "next-auth/client";
+import { useSession, signOut } from "next-auth/client";
 
 import classes from "./main-navigation.module.css";
 
 function MainNavigation() {
   const [session, loading] = useSession();
 
-  console.log(session, loading);
+  function logoutHandler() {
+    // signOut을 호출하면 Promise를 반환하여 처리가 완료되었음을 알려주지만,
+    // useSession을 사용하기 때문에 활성화된 세션에 변화가 있을 때, 컴포넌트가 자동으로 업데이트되고 로그아웃 때도 마찬가지로 업데이트 된다.
+    signOut();
+  }
 
   return (
     <header className={classes.header}>
@@ -29,7 +33,7 @@ function MainNavigation() {
           )}
           {session && (
             <li>
-              <button>Logout</button>
+              <button onClick={logoutHandler}>Logout</button>
             </li>
           )}
         </ul>
