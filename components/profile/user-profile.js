161f85ebf6aea594493614422147c8ffb86b9260
김진영 <jinyoung01099@gmail.com>
2023-05-15 17:39:18 +0900

    [Add] 프로필 페이지 페이지 가드 추가

--- components/profile/user-profile.js
+++ components/profile/user-profile.js
@@ -1,8 +1,28 @@
-import ProfileForm from './profile-form';
-import classes from './user-profile.module.css';
+import { getSession } from "next-auth/client";
+import { useState } from "react";
+
+import ProfileForm from "./profile-form";
+import classes from "./user-profile.module.css";
+import { useEffect } from "react";
 
 function UserProfile() {
-  // Redirect away if NOT auth
+  const [isLoading, setIsLoading] = useState(true);
+  // const [loadedSession, setLoadedSession] = useState();
+
+  useEffect(() => {
+    getSession().then((session) => {
+      if (!session) {
+        window.location.href = "/auth";
+      } else {
+        setIsLoading(false);
+        // setLoadedSession(session);
+      }
+    });
+  }, []);
+
+  if (isLoading) {
+    return <p className={classes.profile}>Loading..</p>;
+  }
 
   return (
     <section className={classes.profile}>
